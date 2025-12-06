26e950afb84cab2814c30f1e63c047d8984547a8
Mihi5543 <mihael.milic19@gmail.com>
2025-12-06 20:02:52 +0100

    Push with changes

---
+++ .idea/battleship.js
@@ -0,0 +1,32 @@
+let location1 = 3;
+let location2 = 4;
+let location3 = 5;
+let guess;
+let hits = 0;
+let guesses = 0;
+let isSunk = false;
+
+while(isSunk == false) {
+    guess = prompt("Guess the position of the battleship (enter a number from 0-6):");
+
+    if (guess < 0 || guess > 6) {
+        alert("Please enter a valid cell number!");
+    } else {
+        guesses += 1;
+
+        if (guess == location1 || guess == location2 || guess == location3) {
+            hits += 1;
+            alert("You hit the battleship!");
+
+            if (hits == 3) {
+                isSunk = true;
+                alert("You sank my battleship!")
+            }
+        } else {
+            alert("Miss!");
+        }
+    }
+}
+let stats = "You sank the battleship in " + guesses + " guesses, which means your shooting accuracy was " + (3/guesses) * 100 + "%";
+
+alert(stats);
